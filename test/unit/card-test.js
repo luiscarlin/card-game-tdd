ef4c0042b1e1ae0949ff0452e6d030b1ae144b92
Luis Carlin <luiscarlin@gmail.com>
2017-11-08 06:30:28 -0500

    Should return a card

--- test/unit/card-test.js
+++ test/unit/card-test.js
@@ -1,7 +1,9 @@
 import { card } from '../../src/card.js'
+import { expect } from 'chai'
 
 describe('card', () => {
   it('should not be null', () => {
     let newCard = card
+    expect(typeof (newCard)).to.not.be.equal('undefined')
   })
 })
